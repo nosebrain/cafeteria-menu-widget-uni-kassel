5c505db06b95e83b1e201f5d662076dafb23ecca
Daniel Zoller <nosebrain@gmx.net>
2010-01-07 22:32:12 +0100

    started refactoring

--- Cafeteria Menu - University of Kassel.dcproj/project/widget.wdgt/js/model/CafeteriaFactory.js
+++ Cafeteria Menu - University of Kassel.dcproj/project/widget.wdgt/js/model/CafeteriaFactory.js
@@ -1,18 +1,18 @@
-var CAFETERIAS_ID = "Cafeterias";
-var BASE_URL_ID = "CafeteriaBaseAddress";
-var CAF_NAME_ID = "Name";
-var CAF_ADDRESS_ID = "Address";
-
 function CafeteriaFactory() {
   reader = new InfoPlistReader();
   
-  this.cafeteriaData = reader.get(CAFETERIAS_ID);
-  this.baseURL = reader.get(BASE_URL_ID);
+  this.cafeteriaData = reader.get("Cafeterias");
+  this.baseURL = reader.get("CafeteriaBaseAddress");
 }
 
 
 CafeteriaFactory.prototype.getCafeteriaById = function(id) {
-  cafeteria = this.cafeteriaData[id];
+  cafeteriaInfo = this.cafeteriaData[id];
   
-  return new Cafeteria(cafeteria[CAF_NAME_ID], this.baseURL + cafeteria[CAF_ADDRESS_ID])
+  cafeteria = new Cafeteria();
+  cafeteria.setName(cafeteriaInfo["Name"]);
+  cafeteria.setURL(this.baseURL + cafeteriaInfo["Address"]);
+  cafeteria.setMenu(new Menu());
+  
+  return cafeteria;
 }
