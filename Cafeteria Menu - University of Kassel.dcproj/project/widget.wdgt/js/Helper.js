b3fbf2ab1ffe13111efe5da8e869e08ac8bf4e5e
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2010-09-01 22:46:08 +0200

    fixed html code remove code

--- Cafeteria Menu - University of Kassel.dcproj/project/widget.wdgt/js/Helper.js
+++ Cafeteria Menu - University of Kassel.dcproj/project/widget.wdgt/js/Helper.js
@@ -23,30 +23,10 @@
  * @author Daniel Zoller<nosebrain@gmx.net>
  */
 
-// FIXME a better implementation of HTMLCode replacment
-function HTMLCodeReplacer(htmlCode, replace) {
-  this.htmlCode = htmlCode;
-  this.replace = replace;
-}
-
-var HTMLCodes = new Array(
-  new HTMLCodeReplacer(/<font face="Arial" size="1">/g, ""),
-  new HTMLCodeReplacer(/<\/font>/g, ""),
-  new HTMLCodeReplacer(/<\/td>/g, ""),
-  new HTMLCodeReplacer(/&nbsp;/g, " "),
-  new HTMLCodeReplacer(/<br \/>/g, " "),
-  new HTMLCodeReplacer(/<\/span>/g, " "),
-  new HTMLCodeReplacer(/<span class="detail">/g, ""),
-  new HTMLCodeReplacer(/  /, " ")
-);
-
-function removeHTMLCode(source) {
-  for (i = 0; i < HTMLCodes.length; i++) {
-    var htmlCodeReplacer = HTMLCodes[i];
-    source = source.replace(htmlCodeReplacer.htmlCode, htmlCodeReplacer.replace);
-  }
-  
-  return source;
+function removeHTMLCode(text) {
+   var tmp = document.createElement("DIV");
+   tmp.innerHTML = text;
+   return tmp.textContent || tmp.innerText;
 }
 
 function myParseInt(string) {
